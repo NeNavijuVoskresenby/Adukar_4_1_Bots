90d1daff6fde459473f3451aac98d0ebf6613d6a
gortatushka <gortatushka@gmail.com>
2021-05-22 19:10:48 +0300

    Антон лох

--- src/main/java/by/adukar/telegrambot/Bot.java
+++ src/main/java/by/adukar/telegrambot/Bot.java
@@ -12,13 +12,15 @@ import by.adukar.telegrambot.service.TextService;
 import by.adukar.telegrambot.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.SneakyThrows;
-import org.telegram.telegrambots.api.methods.send.*;
-import org.telegram.telegrambots.api.objects.Message;
-import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
+import org.telegram.telegrambots.meta.api.methods.send.*;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
 
 
 public class Bot extends TelegramLongPollingBot {
@@ -43,30 +45,67 @@ public class Bot extends TelegramLongPollingBot {
             if (update.getCallbackQuery().getData().equals("Текст для кнопки")) {
                 sendMsg("Обработка инлайн кнопки", chatIdFromCallBack);
             }
-            if (update.getCallbackQuery().getData().equals("Самые красивые места мира")){
-                sendMsgWithButtons("ihugigiuyg", inlineButtons.keyboardMarkup("1", "2", "3"), chatIdFromCallBack);
-                send
-                /* sendMsg("1",chatIdFromCallBack);
-                sendPhoto("https://vandruy.by/wp-content/uploads/2018/08/201302121832369554-min-1024x683.jpeg", chatIdFromCallBack);
-          */  }
+            if (update.getCallbackQuery().getData().equals("Пекин")) {
+                sendMsg("Пекин", chatIdFromCallBack);
+               sendPhoto("https://vandruy.by/wp-content/uploads/2018/08/201302121832369554-min-1024x683.jpeg", chatIdFromCallBack);
+                ;
+            }
 
 
-        }else {
+            if (update.getCallbackQuery().getData().equals("Самые красивые места мира")) {
+                sendMsgWithButtons("Выбирайте место", inlineButtons.keyboardMarkup("Пекин" , "Дубай" , "Лондон"), chatIdFromCallBack);
+                }
+            if (update.getCallbackQuery().getData().equals("Дубай")) {
+                sendMsg("Дубай", chatIdFromCallBack);
+                sendPhoto("https://q-xx.bstatic.com/xdata/images/hotel/max500/185219896.jpg?k=3d4cfc7f2a2e62fb2612b2973d1146bab1c3f40a348894b1a8c04a6ec5946d01&o=", chatIdFromCallBack);
+            }
+            if (update.getCallbackQuery().getData().equals("Лондон")) {
+                sendMsg("Лондон", chatIdFromCallBack);
+                sendPhoto("https://cdn24.img.ria.ru/images/155635/45/1556354505_0:438:2000:1563_1920x0_80_0_0_64fb6d242c6240d9e44e5de1a31a7d6f.jpg", chatIdFromCallBack);
+            }
 
-        Long chatId = update.getMessage().getChatId();
-        if (update.getMessage().getText().equals("/start")) {
-            sendMsg("Начнём работать!", chatId);
-            sendMsgWithButtons("Нажмите на кнопки ниже перечисленные", inlineButtons.keyboardMarkup("Самые красивые места мира"), chatId);
+        } else {
+
+            Long chatId = update.getMessage().getChatId();
+            if (update.getMessage().getText().equals("Голосование")) {
+                sendPoll(chatId);
+            }
+            if (update.getMessage().getText().equals("Кубик")) {
+                sendDice(chatId);
+            }
+            if (update.getMessage().getText().equals("/start")) {
+                sendMsg("Начнём работать!", chatId);
+                sendMsgWithButtons("Нажмите на кнопки ниже перечисленные", inlineButtons.keyboardMarkup("Самые красивые места мира"), chatId);
+            }
+            if (update.getMessage().getText().equals("Выход назад")) {
+                sendMsg("Вы вернулись в первоначальное меню", chatId);
+                sendMsgWithButtons("Выбирайте,что хотите узнать,увидеть", replyButtons.keyboardMarkup("Самые красивые места мира","Кнопка 2"), chatId);
+            }
+
         }
-        if (update.getMessage().getText().equals("Выход назад")) {
-            sendMsg("Вы вернулись в первоначальное меню", chatId);
-            sendMsgWithButtons("Выбирайте,что хотите узнать,увидеть", replyButtons.keyboardMarkup("Самые красивые места мира", "Кнопка 2"), chatId);
+    }
+
+    @SneakyThrows
+    public synchronized void sendPoll(long chatId) {
+        SendPoll sendPoll = new SendPoll();
+        sendPoll.enableNotification();
+        sendPoll.setQuestion("Какая картинка вам понравилась во время просмотра? :)");
+        sendPoll.setAnonymous(true);
+        sendPoll.setOptions(List.of("Пекин", "Дубай", "Лондон"));
+        sendPoll.setChatId(chatId);
+        sendPoll.setCorrectOptionId(2);
+        execute(sendPoll);
+    }
+    @SneakyThrows
+    public synchronized void sendDice(long chatId){
+        SendDice sendDice = new SendDice();
+        sendDice.setChatId(chatId);
+        try {
+            execute(sendDice);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
         }
-
     }
-    }
-
-
 
     public synchronized void sendMsg(String message, Long chatId) {
         SendMessage sendMessage = new SendMessage();
@@ -122,7 +161,7 @@ public class Bot extends TelegramLongPollingBot {
         sendPhotoRequest.setChatId(chatId);
         sendPhotoRequest.setPhoto(pathToPhoto);
         try {
-            sendPhoto(sendPhotoRequest);
+            execute(sendPhotoRequest);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
--- src/main/java/by/adukar/telegrambot/buttons/reply/ReplyButtons.java
+++ src/main/java/by/adukar/telegrambot/buttons/reply/ReplyButtons.java
@@ -2,8 +2,8 @@ package by.adukar.telegrambot.buttons.reply;
 
 import by.adukar.telegrambot.consts.Paths;
 import by.adukar.telegrambot.service.TextService;
-import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -23,6 +23,7 @@ public class ReplyButtons {
         keyboardMarkup.setOneTimeKeyboard(false);
             keyboardFirstRow.add(text1);
             keyboardFirstRow.add(text2);
+
         keyboard.add(keyboardFirstRow);
         keyboardMarkup.setKeyboard(keyboard);
         return keyboardMarkup;
